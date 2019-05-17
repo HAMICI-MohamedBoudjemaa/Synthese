c8753e70dc65438cacd1473254364be9ddf850e4
yoba2419 <mohamed2bayo@yahoo.com>
2019-05-17 14:23:36 +0200

    recupérer des tendances et retourner les tweets correspondants

--- requeteMongo.py
+++ requeteMongo.py
@@ -15,13 +15,10 @@ def getTweetAllTrend():
     for tr in getAllTrend():
         trend = getTweetByTrend(tr)
         tweet_text =[]
-        i = 0
         for tend in trend:
             tweet = tend['tweet_text']
             tweet_text.append(tweet)
-            i = i +1
             result = ' '.join(tweet_text)
-        print("total {} {}".format(tr,i))
     return result
 
 #Structure champs monfo
@@ -39,14 +36,4 @@ def getTweetAllTrend():
     "retweet_count": 
     "created": 
 """
-if __name__ == '__main__':
-   """ i = 0
-    for doc in getTweetByTrend('#Tech4Good'):
-        print(doc['hashtags'][0]['text'])
-        i = i +1
-    print(i)"""
-   #print(getAllTrend())
-   #print(getTweetAllTrend())
-   for t in getTweetByTrend("#VivaTech"):
-        print(t['tweet_text'])
 
