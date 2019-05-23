5c3bc7f6b5ba26d17e2a2b1481a3b863f42cc9ee
yoba2419 <mohamed2bayo@yahoo.com>
2019-05-24 00:33:22 +0200

    extraction des données du flux rss

--- Python/connnexionMongo.py
+++ Python/connnexionMongo.py
@@ -1,8 +1,14 @@
 import pymongo
 from pymongo import MongoClient
 
+"""local = MongoClient('127.0.0.1', 27017)
+db = local.WebSensors
+tweets = db.tweets
+events = db.events"""
+
 
 client = pymongo.MongoClient("mongodb+srv://bayo:rien@cluster0-gn4j1.mongodb.net/test?retryWrites=true")
 collection = client.projet
 tweets = collection.tweets
 events = collection.events
+fluxRSS = collection.fluxRSS
