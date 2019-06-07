d2d97b236e65e26494554bae7fb3a1a5045851bd
Hong Phuc <hongphuc95@gmail.com>
2019-06-07 12:29:17 +0200

    Geonames avec les statistiques pour les résultats v1.3 (Conclusion sur la statistique de villes)

--- Python/GeonamesAnalyze.py
+++ Python/GeonamesAnalyze.py
@@ -9,7 +9,7 @@ TOPHITS = 3
 def textProcessing(text):
     text = re.sub(r'\-', ' ', text)
     text = re.sub(r'\"', '', text)
-    text = re.sub(r'\.\s*(?=)[A-Z]', lambda m: m.group().lower(), text)  # cute. This is a phrase -> cute. this is..
+    #text = re.sub(r'\.\s*(?=)[A-Z]', lambda m: m.group().lower(), text)  # cute. This is a phrase -> cute. this is..
     text = re.sub(r'\?\s*(?=)[A-Z]', lambda m: m.group().lower(), text)  # cute? This is a phrase -> cute. this is..
     text = re.sub(r'\!\s*(?=)[A-Z]', lambda m: m.group().lower(), text)  # cute? This is a phrase -> cute. this is..
     text = re.sub(r'\:\s*(?=)[A-Z]', lambda m: m.group().lower(), text)  # cute? This is a phrase -> cute. this is..
@@ -17,18 +17,18 @@ def textProcessing(text):
     #text = re.sub(r'\W\s*(?=)[A-Z]', lambda m: m.group().lower(), text)  # cute! This is a phrase -> cute. this is..
     #text = re.sub('^\s*[A-Z]', lambda m: m.group().lower(), text, flags=re.M)
     placeDict = dict()
-    placeRegex = re.compile(r'([A-Z][A-Za-z]+(\s+[A-Z][a-z]+)*)')
-    hashtagRegex = re.compile(r'\#[A-Za-z0-9]*')
+    placeRegex = re.compile(r'([A-Z][A-Za-zàéèêëîïôûüÿù]+(\s+[A-Z][a-zàéèêëîïôûüÿù]+)*)', re.UNICODE)
+    hashtagRegex = re.compile(r'\#[A-Za-zàéèêëîïôûüÿù0-9]*', re.UNICODE)
     places = placeRegex.findall(text)
     hashtags = hashtagRegex.findall(text)
 
-    stopWordRegexArray = ['\bThe\b', '\bLe\b', '\bLa\b', '\bL\'\b', '\bUn\b', '\bUne\b', \
-                     '\bTa\b', '\bTon\b', '\bSon\b', '\bSa\b', '\bSes\b', '\bVotre\b', '\bNotre\b', \
-                     '\bJe\b', '\bTu\b', '\bNous\b', '\bVous\b', '\bIl\b', '\bElle\b']
+    stopWordArray = ['The', 'Le', 'La', 'L\'', 'Un', 'Une', \
+                     'Ta', 'Ton', 'Son', 'Sa', 'Ses', 'Votre', 'Notre', \
+                     'Je', 'Tu', 'Nous', 'Vous', 'Il', 'Elle']
     for place in places:
         place = place[0].strip()
-        for stopWordRegex in stopWordRegexArray:
-            place = re.sub(stopWordRegex, '', place)
+        #for stopWordRegex in stopWordRegexArray:
+        #    place = re.sub(stopWordRegex, '', place)
         if place.isupper():  # WASHINGTON -> #Washington
              place = place.lower()
              place = place.capitalize()
@@ -40,7 +40,7 @@ def textProcessing(text):
 
     for hashtag in hashtags:
         hashtag = hashtag.replace('#', '')
-        if re.search('^[a-z].*', hashtag):
+        if re.search('^[a-zàéèêëîïôûüÿù].*', hashtag):
             hashtag = hashtag.capitalize()  # notredame -> Notredame
 
         if hashtag in placeDict:
@@ -50,7 +50,7 @@ def textProcessing(text):
 
     newPlaceDict = dict()
     for key,value in placeDict.items():
-        if value > THRESHOLD_KEYWORD:
+        if value > THRESHOLD_KEYWORD and key not in stopWordArray:
             newPlaceDict[key] = value
     placeDict = newPlaceDict
     del(newPlaceDict)
@@ -71,7 +71,7 @@ def analyze(str):
     resultDict['place'] = None
 
     extractedCityCountry = textProcessing(str)
-    print(extractedCityCountry)
+    #print(extractedCityCountry)
     extractedPlace = []
     dictCityCountry = dict()
     dictCityCountry['country'] = None
@@ -121,7 +121,9 @@ def analyze(str):
 
         for place in extractedPlace:
             placename = place[0]
+            #print(placename)
             arrayPlace = searchPlaceByName(placename, dictCityCountry)
+            #print(arrayPlace)
 
             if arrayPlace is not None:
                 for item in arrayPlace:
@@ -156,22 +158,22 @@ def showStatement(statement, lengthTable):
 
 def showResult(arrayResult):
     lengthTable = 58
-    if arrayResult['country'] is not None:
-        print('**********************************************************')
-        print('*                      Pays trouvés                      *')
-        print('**********************************************************')
-        for country in arrayResult['country']:
-            count = arrayResult['country'][country][0]
-            info = arrayResult['country'][country][1]
-            showStatement('* [{}] apparait {} fois dans le document'.format(country, count), lengthTable)
-            showStatement('* [{}] Code Pays: {}'.format(country, info.ofCountry), lengthTable)
-        print('**********************************************************')
-        print('\n')
-    else:
-        print('**********************************************************')
-        print('*            Pas d\'information pour les pays            *')
-        print('**********************************************************')
-        print('\n')
+    # if arrayResult['country'] is not None:
+    #     print('**********************************************************')
+    #     print('*                      Pays trouvés                      *')
+    #     print('**********************************************************')
+    #     for country in arrayResult['country']:
+    #         count = arrayResult['country'][country][0]
+    #         info = arrayResult['country'][country][1]
+    #         showStatement('* [{}] apparait {} fois dans le document'.format(country, count), lengthTable)
+    #         showStatement('* [{}] Code Pays: {}'.format(country, info.ofCountry), lengthTable)
+    #     print('**********************************************************')
+    #     print('\n')
+    # else:
+    #     print('**********************************************************')
+    #     print('*            Pas d\'information pour les pays            *')
+    #     print('**********************************************************')
+    #     print('\n')
 
     if arrayResult['city'] is not None:
         print('**********************************************************')
@@ -191,26 +193,27 @@ def showResult(arrayResult):
         print('\n')
 
 
-    if arrayResult['place'] is not None:
-        print('**********************************************************')
-        print('*                     Lieux trouvés                      *')
-        print('**********************************************************')
-        for place in arrayResult['place']:
-            count = arrayResult['place'][place][0]
-            info = arrayResult['place'][place][1]
-            showStatement('* [{}] apparait {} fois dans le document'.format(place, count), lengthTable)
-            showStatement('* [{}] Code Pays: {}'.format(place, info.ofCountry), lengthTable)
-            showStatement('* [{}] Villes: {}'.format(place, searchCityByCode(info.ofCity)), lengthTable)
-        print('**********************************************************')
-        print('\n')
-    else :
-        print('**********************************************************')
-        print('*            Pas d\'information pour les lieux            *')
-        print('**********************************************************')
-        print('\n')
+    # if arrayResult['place'] is not None:
+    #     print('**********************************************************')
+    #     print('*                     Lieux trouvés                      *')
+    #     print('**********************************************************')
+    #     for place in arrayResult['place']:
+    #         count = arrayResult['place'][place][0]
+    #         info = arrayResult['place'][place][1]
+    #         showStatement('* [{}] apparait {} fois dans le document'.format(place, count), lengthTable)
+    #         showStatement('* [{}] Code Pays: {}'.format(place, info.ofCountry), lengthTable)
+    #         showStatement('* [{}] Villes: {}'.format(place, searchCityByCode(info.ofCity)), lengthTable)
+    #     print('**********************************************************')
+    #     print('\n')
+    # else :
+    #     print('**********************************************************')
+    #     print('*            Pas d\'information pour les lieux            *')
+    #     print('**********************************************************')
+    #     print('\n')
 
 def analyzeResult(arrayResult):
-    print('Pays statistique')
+    relationalDict = dict()
+    # print('Pays statistique')
     countryStat = None
     if arrayResult['country'] is not None:
         countryStat = dict()
@@ -222,30 +225,60 @@ def analyzeResult(arrayResult):
         prevContinent = None
         for country in arrayResult['country']:
             info = arrayResult['country'][country][1]
-            continent = searchContinentByISO(info.ofCountry)
+            continentBelong = searchContinentByISO(info.ofCountry)
             if prevContinent is None:
-                prevContinent = continent
-            countryStat[country] = [(arrayResult['country'][country][0]/totalCountCountry*100), continent, info.ofCountry]
-            if continent != prevContinent:
+                prevContinent = continentBelong
+            countryStat[country] = [(arrayResult['country'][country][0]/totalCountCountry*100), continentBelong, info.ofCountry]
+            if continentBelong != prevContinent:
                 sameContinent = False
-            prevContinent = continent
+            prevContinent = continentBelong
 
-        if countryStat:
-            if sameContinent is True and len:
-                print('Les pays ci-dessus se situent dans le meme continent {}'.format(prevContinent))
+        # if countryStat:
+        #     if sameContinent is True and len(countryStat) > 1:
+        #         print('Les pays ci-dessus se situent dans le meme continent {}'.format(prevContinent))
 
-            for country in countryStat:
-                if countryStat[country][0] > 60:
-                    print('[{}] occupe une partie importante de résultat de {}%'.format(country, countryStat[country][0]))
+            # for country in countryStat:
+            #     if countryStat[country][0] > 60:
+            #         print('Le pays [{}] occupe une partie importante de résultat de {}%'.format(country, countryStat[country][0]))
 
     print('Villes statistique')
     if arrayResult['city'] is not None:
+        cityStat = dict()
+        totalCountCity = 0
+        sameCountry = True
+        for city in arrayResult['city']:
+            totalCountCity += arrayResult['city'][city][0]
+
+        prevCountry = None
         for city in arrayResult['city']:
             info = arrayResult['city'][city][1]
+            countryBelong = info.ofCountry
+
+            if prevCountry is None:
+                prevCountry = countryBelong
+            cityStat[city] = [(arrayResult['city'][city][0]/totalCountCity*100), countryBelong, info.ofCountry]
+            if countryBelong != prevCountry:
+                sameCountry = False
+            prevCountry = countryBelong
+
             if countryStat:
                 for country in countryStat:
                     if info.ofCountry == countryStat[country][2]:
-                        print('[{}] appartient à {}'.format(city, country))
+                        relationalDict[city] = [country]
+                        print('[{}] appartient à {} trouvé dans le résultat'.format(city, country))
+                        print('La ville [{}] occupe une partie de {}% de résultat trouvé'.format(city,
+                                                                                                        cityStat[city][0]))
+
+        # if cityStat:
+        #     if sameCountry is True and len(cityStat) > 1:
+        #         print('Les villes ci-dessus se situent dans le meme pays {}'.format(prevCountry))
+        #
+        #     for city in cityStat:
+        #         if cityStat[city][0] > 60:
+        #             print('La ville [{}] occupe une partie importante de résultat de {}%'.format(city,
+        #                                                                                 cityStat[city][0]))
+
+
 
 
 
